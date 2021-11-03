1a9a88afbdcf639b62011fdba84266265962def5
PavanBandichode <pavan.bandichode@gmail.com>
2021-11-03 21:05:11 +0530

    alert promt confirm assignmen1

--- block-BJaaao/code/index.js
+++ block-BJaaao/code/index.js
@@ -16,11 +16,16 @@ let num2 = 32;
 - Alert `The value of num1 is [num1] and the value of num2 is [num2]`
 - Alert `The sum of [num1] and [num2] is [num1 + num1]`
 */
+let language="JavaScript";
+alert(`I am learning ${language}`);
+alert(`value of num1 is ${num1} and value of num2 is ${num2}`);
+alert(`The sum of num1 and num2 is ${num1+num2}`);
 
 /*
 2. Do the following using `alert`, `prompt` and `confirm` and `console`
 
   - Using prompt accept the name of the user and store it a variable names `userName`
+  
   - Using prompt accept the profession of the user and store it a variable names`userProfession`
   - Using confirm check if user is adult or not and store the value in `isAdult`
   - Alert message `I am [userName] a [userProfession]`
@@ -32,7 +37,20 @@ let num2 = 32;
     Adult: [isAdult]
     Profession: [userProfession]` (it should be in three different lines)
 */
+// prompt("enter your name :");
+// let userName=prompt("enter your name :");
 
+// prompt("enter your profession :");
+// let userProfession=prompt("enter your profession :");
+// alert(`I am ${userName} a ${userProfession}`);
+
+
+// prompt("Are you adult?");
+// let isAdult=prompt("Are you adult?");
+
+// alert(`Username: ${userName} \nAdult: ${isAdult} \nProfession: ${userProfession}`)
+    
+   
 /*
 . Addition using prompt
 
@@ -43,3 +61,8 @@ let num2 = 32;
   - Log using console.log [numA - numB]
   - Log using console.log [numA * numB]
 */
+let numA = 21;
+let numB = 32;
+alert(numA+numB);
+alert(numA-numB);
+alert(numA*numB);
